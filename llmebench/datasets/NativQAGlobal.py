ea150111fd88246074f94637dbeb4b9a0dbb906b
hunzed <101815330+hunzed@users.noreply.github.com>
2025-06-11 11:28:41 +0300

    Scripts for testing on NativGlobal dataset

--- llmebench/datasets/NativQAGlobal.py
+++ llmebench/datasets/NativQAGlobal.py
@@ -84,7 +84,85 @@ class NativQAGlobalDataset(DatasetBase):
                     "dev": "quebec/nativqa_dev.tsv",
                     "test": "quebec/nativqa_test.tsv",
                 },
+
+                # for georgia
+                "georgia": {
+                    "dev": "georgia/nativqa_dev.tsv",
+                    "test": "georgia/nativqa_test.tsv",
+                },
+
+                # for iraq
+                "iraq": {
+                    "dev": "Iraq_Baghdad/nativqa_dev.tsv",
+                    "test": "Iraq_Baghdad/nativqa_test.tsv",
+                },
+
+                # for mauritania
+                "mauritania": {
+                    "dev": "Mauritania_Nouakchott/nativqa_dev.tsv",
+                    "test": "Mauritania_Nouakchott/nativqa_test.tsv",
+                },
+
+                # for kuwait
+                "kuwait": {
+                    "dev": "Kuwait_Kuwait_City/nativqa_dev.tsv",
+                    "test": "Kuwait_Kuwait_City/nativqa_test.tsv",
+                },
+
+                # for lebanon
+                "lebanon": {
+                    "dev": "Lebanon_Beirut/nativqa_dev.tsv",
+                    "test": "Lebanon_Beirut/nativqa_test.tsv",
+                },
+
+                # for libya
+                "libya": {
+                    "dev": "Libya_Tripoli/nativqa_dev.tsv",
+                    "test": "Libya_Tripoli/nativqa_test.tsv",
+                },
             
+                # for morocco
+                "morocco": {
+                    "dev": "Morocco_Rabat/nativqa_dev.tsv",
+                    "test": "Morocco_Rabat/nativqa_test.tsv",
+                },
+
+                # for oman
+                "oman": {
+                    "dev": "Oman_Muscat/nativqa_dev.tsv",
+                    "test": "Oman_Muscat/nativqa_test.tsv",
+                },
+
+                # for saudia arabia riyadh
+                "saudi_arabia": {
+                    "dev": "Saudi_Arabia_Riyadh/nativqa_dev.tsv",
+                    "test": "Saudi_Arabia_Riyadh/nativqa_test.tsv",
+                },
+
+                # for uae abu dhabi
+                "uae": {
+                    "dev": "UAE_Abu_Dhabi/nativqa_dev.tsv",
+                    "test": "UAE_Abu_Dhabi/nativqa_test.tsv",
+                },
+
+                # for algeria
+                "algeria": {
+                    "dev": "Algeria_Algiers/nativqa_dev.tsv",
+                    "test": "Algeria_Algiers/nativqa_test.tsv",
+                },
+
+                # for egypt
+                "egypt": {
+                    "dev": "Egypt_Cairo/nativqa_dev.tsv",
+                    "test": "Egypt_Cairo/nativqa_test.tsv",
+                },
+
+                # for bahrain
+                "bahrain": {
+                    "dev": "Bahrain_Manama/nativqa_dev.tsv",
+                    "test": "Bahrain_Manama/nativqa_test.tsv",
+                },
+                
                 "default": [
                 ],
             },
