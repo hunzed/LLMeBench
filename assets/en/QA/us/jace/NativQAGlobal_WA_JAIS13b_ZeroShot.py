d410d0b103aa8c14c182e95c9899dd6f9bcb3f97
hunzed <101815330+hunzed@users.noreply.github.com>
2025-06-16 15:41:26 +0300

    jace testing

---
+++ assets/en/QA/us/jace/NativQAGlobal_WA_JAIS13b_ZeroShot.py
@@ -0,0 +1,38 @@
+import json
+import re
+
+from llmebench.datasets import NativQAGlobalDataset
+from llmebench.models import AzureModel
+from llmebench.tasks import MultiNativQATask
+
+
+def metadata():
+    return {
+        "author": "Arabic Language Technologies, QCRI, HBKU",
+        "model": "Jais-13b-chat",
+        "description": "",
+        "scores": {},
+    }
+
+
+def config():
+    return {
+        "dataset": NativQAGlobalDataset,
+        "task": MultiNativQATask,
+        "model": AzureModel,
+        "general_args": {"test_split": "washington"},
+    }
+
+
+def prompt(input_sample):
+    base_prompt = f"Your task is to answer questions in English based on a given context.\nNote: Your answers should be spans extracted from the given context without any illustrations.\nYou don't need to provide a complete answer\nContext:{input_sample['context']}\nQuestion:{input_sample['question']}\nAnswer:"
+    
+    return [
+        {
+            "role": "user",
+            "content": base_prompt,
+        },
+    ]
+
+def post_process(response):
+    return response["choices"][0]["message"]["content"]
